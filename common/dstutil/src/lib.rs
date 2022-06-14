dc9706bf4f94c67055739fe005e0cffd7d961d45
Chloé Kekoa <chloekek@use.startmail.com>
2022-06-14 20:28:32 +0200

    dstutil: Add convenient CustomDst type

--- common/dstutil/src/lib.rs
+++ common/dstutil/src/lib.rs
@@ -1,3 +1,108 @@
-//! Utility for working with custom dynamically-sized types.
+//! Utilities for working with custom dynamically-sized types.
+
+#![feature(allocator_api)]
+#![feature(layout_for_ptr)]
+#![feature(ptr_metadata)]
+#![warn(missing_docs)]
+
+use {
+    scope_exit::ScopeExit,
+    std::{
+        alloc::{AllocError, Allocator, Global, Layout},
+        mem::forget,
+        ptr::{Pointee, addr_of_mut, from_raw_parts, from_raw_parts_mut, null},
+    },
+};
 
 pub mod thin;
+
+/// Custom dynamically-sized type.
+///
+/// Consists of a sized head `H` and a dynamically-sized tail `T`.
+#[allow(missing_docs)]
+pub struct CustomDst<H, T>
+    where T: ?Sized
+{
+    pub head: H,
+    pub tail: T,
+}
+
+impl<H, T> CustomDst<H, T>
+    where T: ?Sized
+{
+    /// Allocate and initialize a custom dynamically-sized value on the heap.
+    ///
+    /// The `tail_metadata` argument becomes the metadata for the fat pointer.
+    /// The `tail_init` function initializes the tail of the value.
+    /// If `tail_init` panics, the tail is not dropped.
+    ///
+    /// # Safety
+    ///
+    /// `tail_metadata` must be suitable for the value
+    /// that `tail_init` is going to initialize.
+    /// When `tail_init` returns, the tail must be initialized.
+    pub unsafe fn new_boxed(
+        head: H,
+        tail_metadata: <Self as Pointee>::Metadata,
+        tail_init: impl FnOnce(*mut T),
+    ) -> Box<Self>
+    {
+        Self::new_boxed_in(head, tail_metadata, tail_init, Global)
+    }
+
+    /// See [`new_boxed`][`Self::new_boxed`].
+    pub unsafe fn new_boxed_in<A>(
+        head: H,
+        metadata: <Self as Pointee>::Metadata,
+        tail_init: impl FnOnce(*mut T),
+        alloc: A,
+    ) -> Box<Self, A>
+        where A: Allocator
+    {
+        match Self::try_new_boxed_in(head, metadata, tail_init, alloc) {
+            Ok(boxed) => boxed,
+            Err(_) => todo!("Call handle_alloc_error with correct layout"),
+        }
+    }
+
+    /// See [`new_boxed`][`Self::new_boxed`].
+    pub unsafe fn try_new_boxed(
+        head: H,
+        tail_metadata: <Self as Pointee>::Metadata,
+        tail_init: impl FnOnce(*mut T),
+    ) -> Result<Box<Self>, AllocError>
+    {
+        Self::try_new_boxed_in(head, tail_metadata, tail_init, Global)
+    }
+
+    /// See [`new_boxed`][`Self::new_boxed`].
+    pub unsafe fn try_new_boxed_in<A>(
+        head: H,
+        metadata: <Self as Pointee>::Metadata,
+        tail_init: impl FnOnce(*mut T),
+        alloc: A,
+    ) -> Result<Box<Self, A>, AllocError>
+        where A: Allocator
+    {
+        // Compute the layout for the dynamically-sized value.
+        let dummy_ptr = from_raw_parts::<Self>(null(), metadata);
+        // FIXME: This is currently unsafe because Layout::for_value_raw
+        //        requires the size to fit in isize and we don't check that.
+        let layout = Layout::for_value_raw(dummy_ptr);
+
+        // Allocate memory for the dynamically-sized value.
+        let ptr = alloc.allocate(layout)?.cast::<u8>();
+        let fat = from_raw_parts_mut::<Self>(ptr.as_ptr().cast(), metadata);
+
+        // Initialize the tail first, so that head is dropped on panic.
+        let init_guard = ScopeExit::new(|| alloc.deallocate(ptr, layout));
+        tail_init(addr_of_mut!((*fat).tail));
+        forget(init_guard);
+
+        // Initialize the head.
+        (*fat).head = head;
+
+        // Create the box to be returned.
+        Ok(Box::from_raw_in(fat, alloc))
+    }
+}
--- common/dstutil/src/thin/example.rs
+++ common/dstutil/src/thin/example.rs
@@ -1,15 +1,11 @@
-# #![feature(layout_for_ptr)]
 # #![feature(ptr_metadata)]
 #
-# use dstutil::thin::{EnableThin, ThinRef};
-use std::{mem::size_of, ptr::from_raw_parts};
+use dstutil::{CustomDst, thin::{EnableThin, ThinRef}};
+use std::{mem::{size_of, transmute}, ptr::{copy_nonoverlapping, from_raw_parts}};
 
 /// String that is prefixed with its length.
-pub struct PascalStr
-{
-    len: usize,
-    data: str,
-}
+#[repr(transparent)]
+pub struct PascalStr(CustomDst<usize, str>);
 
 unsafe impl EnableThin for PascalStr
 {
@@ -20,54 +16,30 @@ unsafe impl EnableThin for PascalStr
         let dummy = from_raw_parts::<Self>(this, 0);
 
         // Recreate the pointer but with the correct length.
-        from_raw_parts(this, (*dummy).len)
+        from_raw_parts(this, (*dummy).0.head)
     }
 }
 
 // Function for creating a PascalStr object on the heap.
 fn new_pascal_str(s: &str) -> Box<PascalStr>
 {
-    // How to initialize custom DSTs is complicated but also irrelevant,
-    // so the lengthy body of this function is hidden from the example.
-#   use std::{
-#       alloc::{Layout, alloc, handle_alloc_error},
-#       ptr::{addr_of_mut, copy_nonoverlapping, from_raw_parts_mut, null},
-#   };
-#   unsafe {
-#       // Compute the layout for the PascalStr object.
-#       // FIXME: for_value_raw requires size to fit in isize.
-#       let dummy_ptr = from_raw_parts::<PascalStr>(null(), s.len());
-#       let layout = Layout::for_value_raw(dummy_ptr);
-#
-#       // Allocate memory for the PascalStr object.
-#       let ptr = alloc(layout);
-#       if ptr.is_null() {
-#           handle_alloc_error(layout);
-#       }
-#
-#       // Initialize the allocated memory.
-#       let ptr = from_raw_parts_mut::<PascalStr>(ptr.cast(), s.len());
-#       (*ptr).len = s.len();
-#       copy_nonoverlapping(
-#           /* src */ s.as_ptr(),
-#           /* dst */ addr_of_mut!((*ptr).data).cast(),
-#           /* len */ s.len(),
-#       );
-#
-#       // Create a box for safe usage.
-#       Box::from_raw(ptr)
-#   }
+    unsafe {
+        let inner = CustomDst::<usize, str>::new_boxed(s.len(), s.len(), |d| {
+            copy_nonoverlapping::<u8>(s.as_ptr(), d.cast(), s.len());
+        });
+        transmute::<Box<CustomDst<usize, str>>, Box<PascalStr>>(inner)
+    }
 }
 
 // ThinRef<PascalStr> can be used just like &PascalStr!
 let pascal_str_box = new_pascal_str("Hello, world!");
 let pascal_str_thin_ref = ThinRef::new(&pascal_str_box);
-assert_eq!(&pascal_str_thin_ref.data, "Hello, world!");
+assert_eq!(&pascal_str_thin_ref.0.tail, "Hello, world!");
 
 // A normal reference to a Pascal string is two words.
 // One word stores the address, the other word stores the length.
 assert_eq!(size_of::<&PascalStr>(), 2 * size_of::<usize>());
 
 // A thin reference to a Pascal string is only one word!
-// The length can already be found in the PascalStr::len field.
+// The length can already be found in the CustomDst::head field.
 assert_eq!(size_of::<ThinRef<PascalStr>>(), size_of::<usize>());
